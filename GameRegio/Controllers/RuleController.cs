f49d4a6130eb131d875a5f4219218c33b0b16771
mrtagcaa@gmail.com <mrtagcaa@gmail.com>
2020-10-10 00:53:38 +0300

    Rules added

--- GameRegio/Controllers/RuleController.cs
+++ GameRegio/Controllers/RuleController.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GameRegio.Entities;
+using GameRegio.Interface;
+using GameRegio.ServiceContracts.Rule;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 
 namespace GameRegio.Controllers
 {
@@ -11,5 +15,96 @@ namespace GameRegio.Controllers
     [ApiController]
     public class RuleController : ControllerBase
     {
+        private readonly IRuleDataAccess _ruleDataAccess;
+
+        public RuleController(IRuleDataAccess ruleDataAccess)
+        {
+            this._ruleDataAccess = ruleDataAccess;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var result = _ruleDataAccess.Get();
+            if (result == null)
+            {
+                return BadRequest("Not found");
+            }
+
+            return Ok(result.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult GetById(RuleGetByIdModel ruleGetByIdModel)
+        {
+            var result = _ruleDataAccess.GetByIdAsync(ruleGetByIdModel.Id).Result;
+            if (result == null)
+            {
+                return BadRequest("Not found");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Rules rules)
+        {
+            var rule = _ruleDataAccess.AddAsync(new Rules()
+            {
+                Id = ObjectId.GenerateNewId().ToString(),
+                RuleName = rules.RuleName,
+                RuleDescription = rules.RuleDescription,
+                GameId = rules.GameId,
+                ModId = rules.ModId,
+                IsActive = rules.IsActive
+
+            });
+
+            if (rule == null)
+                return BadRequest(new { message = "Rule eklenemedi!" });
+
+            return Ok(new { rule.Result });
+        }
+
+        [HttpPost]
+        public IActionResult Update(RuleUpdateModel ruleUpdateModel)
+        {
+            try
+            {
+
+                var rule = _ruleDataAccess.UpdateAsync(ruleUpdateModel.RuleId.ToString(), new Rules()
+                {
+                    RuleName = ruleUpdateModel.RuleName,
+                    RuleDescription = ruleUpdateModel.RuleDescription,
+                    GameId = ruleUpdateModel.GameId,
+                    ModId = ruleUpdateModel.ModId,
+                    IsActive = ruleUpdateModel.IsActive
+
+                }).Result;
+
+                rule = _ruleDataAccess.GetByIdAsync(ruleUpdateModel.RuleId.ToString()).Result;
+
+                if (rule == null)
+                    return BadRequest(new { message = "İstatistik güncellenemedi!" });
+
+                return Ok(new { rule });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(RuleDeleteModel ruleDeleteModel)
+        {
+            var result = _ruleDataAccess.DeleteAsync(ruleDeleteModel.Id).Result;
+            if (result == null)
+            {
+                return BadRequest("İstatistik silinemedi!");
+            }
+
+            return Ok(new { result });
+        }
     }
 }
---
+++ GameRegio/ServiceContracts/Rule/RuleUpdateModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameRegio.ServiceContracts.Rule
+{
+    public class RuleUpdateModel
+    {
+        public string RuleId { get; set; }
+        public string RuleName { get; set; }
+        public string RuleDescription { get; set; }
+        public string GameId { get; set; }
+        public string ModId { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
